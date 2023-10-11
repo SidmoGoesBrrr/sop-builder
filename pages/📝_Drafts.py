af30daf6cee223846d41696f4783e8b92c14fa5c
Jason Dsouza <76958526+Mercurius13@users.noreply.github.com>
2023-10-11 19:51:27 +0530

    Update 📝_Drafts.py

--- pages/📝_Drafts.py
+++ pages/📝_Drafts.py
@@ -3,10 +3,13 @@ from pymongo import MongoClient
 import time
 import database
 
+
 def display_drafts_page():
     st.header("Drafts")
+    print(st.session_state.user_id)
     try:
         updated_draft = database.get_user_data_by_id(st.session_state.user_id)['draft']
+        print(updated_draft)
     except:
         updated_draft = None
     if updated_draft is not None:
@@ -15,8 +18,7 @@ def display_drafts_page():
             st.write("-----------------------------------------------------------------------------------------------")
     else:
         st.info("No drafts found!")
-    while not st.button("Back"):
-        time.sleep(3)
+
 
 if st.session_state.get("user_logged_in") == True:
     display_drafts_page()
