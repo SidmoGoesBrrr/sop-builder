80e053c1a3907df98b96b7ac05a2c68956d6ef48
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-10-19 02:39:48 +0530

    Update 📄_SOP.py

--- pages/📄_SOP.py
+++ pages/📄_SOP.py
@@ -173,7 +173,7 @@ if st.session_state.get("user_logged_in") == True:
         sop_display_area.write(str(st.session_state.generated_sop))
     
     if state.section_index == len(text_areas) - 5:
-        if not st.session_state.summary:
+        if "summary" not in st.session_state or st.session_state.summary=="":
             uploaded_file = st.file_uploader("Upload your resume here", type=["pdf"])
             summary=None
             if uploaded_file is not None:
@@ -277,6 +277,7 @@ if st.session_state.get("user_logged_in") == True:
         if state.section_index == len(text_areas) - 1:
             if st.button("Start Over"):
                 state.section_index = 0
+                st.session_state.summary==""
                 st.rerun()
 
         elif state.section_index == len(text_areas) - 2:
