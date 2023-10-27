87715a2a98d3070ccd56b346f29a4064b9f32d5b
Jason Dsouza <76958526+Mercurius13@users.noreply.github.com>
2023-10-27 18:54:53 +0530

    Update 2_🔑_Sign_In.py
    This is the real OTP Fix

--- pages/2_🔑_Sign_In.py
+++ pages/2_🔑_Sign_In.py
@@ -13,13 +13,14 @@ from database import get_user_data, update_user
 import pytz
 import streamlit as st
 import os
-#####################################################################################################################################################
 from streamlit.components.v1 import html
+
 
 @st.cache_resource(ttl=1200)
 def get_users_collection():
     users_collection = database.users_collection
     return users_collection
+
 
 def nav_page(page_name, timeout_secs=3):
     nav_script = """
@@ -51,17 +52,17 @@ def send_otp(phone_number, generated_otp):
     url = 'https://iqsms.airtel.in/api/v1/send-sms'
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': st.secrets['sms_auth']
+        'Authorization': st.secrets["sms_auth"],
     }
 
     data = {
-        "customerId": st.secrets['customerId'],
+        "customerId": st.secrets["customerId"],
         "destinationAddress": f"{str(phone_number)}",
         "message": f"{str(generated_otp)} is your OTP to login to Vidyalankar's AdmitAbroad webapp.",
         "sourceAddress": "IVIDYA",
         "messageType": "SERVICE_EXPLICIT",
-        "dltTemplateId": st.secrets['dltTemplateId'],
-        "entityId": st.secrets['entityId'],
+        "dltTemplateId": st.secrets["dltTemplateId"],
+        "entityId": st.secrets["entityId"]
     }
 
     response = requests.post(url, headers=headers, json=data)
@@ -73,6 +74,8 @@ def send_otp(phone_number, generated_otp):
     else:
         print(f"Request failed with status code {response.status_code}:")
         print(response.text)
+
+
 # Call the function with the OTP
 im = Image.open('icon.png')
 
@@ -80,6 +83,8 @@ st.set_page_config(page_title="SOP Generator", page_icon=im)
 
 if 'gen_button' not in st.session_state:
     st.session_state.disabled = True
+if 'login_button' not in st.session_state:
+    st.session_state.login_button = True
 st.header("Sign In")
 username = st.text_input("Name")
 phone_number = st.text_input("Mobile Number", placeholder="9631331342")
@@ -88,48 +93,46 @@ if username is not None and phone_number is not None:
 
 generate_otp = st.button("Generate OTP", disabled=st.session_state.disabled, key='gen_button')
 
-if st.session_state.get('button') != True:
-    st.session_state['button'] = generate_otp
-    st.session_state.disabled = True
 
-if st.session_state['button'] == True:
-    st.session_state.disabled = True
+if generate_otp:
+    st.session_state.disabled = False
     if len(phone_number) != 10 or phone_number is None:
         st.error("Sorry, not a valid phone number")
-        st.session_state.disabled = False
-        st.rerun()
 
     else:
         users_collection = get_users_collection()
         if username in [user['username'] for user in users_collection.find()] and phone_number in [user['phone_number']
                                                                                                    for user in
                                                                                                    users_collection.find()]:
-            generated_otp = random.randint(1000, 9999)
+            st.session_state.generated_otp = random.randint(1000, 9999)
             time.sleep(1)
-            send_otp(phone_number, generated_otp)
+            send_otp(phone_number, st.session_state.generated_otp)
             time.sleep(5)
-            otp = st.text_input("OTP")
-            if st.button('Login'):
-                if otp == generated_otp:
-                    st.success("Logged in Successfully.")
-                    st.session_state.user_logged_in = True
-                    st.session_state.user_id = str(get_user_data(username)['_id'])
-                    print(st.session_state.user_id)
-                    ist = pytz.timezone('Asia/Kolkata')
-
-                    update_user(username=username,
-                                data={"last_logged_in": datetime.now(ist).strftime("%A,%d %B %Y - %H:%M:%S")})
-                    os.rename(r'pages/2_🔑_Sign_In.py', r'lages/2_🔑_Sign_In.py')
-                    os.rename(r'lages/2_🔑_Sign_Out.py', r'pages/2_🔑_Sign_Out.py')
-                    nav_page("")
-
-                else:
-                    st.error("Invalid OTP")
-                    st.session_state.disabled = False
-                    time.sleep(1)
-                    st.rerun()
+            st.success("OTP sent successfully")
+            st.session_state.login_button = False
         else:
             st.error("Invalid User")
             st.session_state.disabled = False
             time.sleep(1)
-            st.rerun()
+
+if not st.session_state.login_button:
+    otp = st.text_input("Enter OTP", key='otp')
+    if st.button('Login'):
+        print(otp, st.session_state.generated_otp)
+        if otp == str(st.session_state.generated_otp):
+            st.success("Logged in Successfully.")
+            st.session_state.user_logged_in = True
+            st.session_state.user_id = str(get_user_data(username)['_id'])
+            print(st.session_state.user_id)
+            ist = pytz.timezone('Asia/Kolkata')
+
+            update_user(username=username,
+                        data={"last_logged_in": datetime.now(ist).strftime("%A,%d %B %Y - %H:%M:%S")})
+            os.rename(r'pages/2_🔑_Sign_In.py', r'lages/2_🔑_Sign_In.py')
+            os.rename(r'lages/2_🔑_Sign_Out.py', r'pages/2_🔑_Sign_Out.py')
+            nav_page("")
+
+        else:
+            st.error("Invalid OTP")
+            st.session_state.disabled = False
+            time.sleep(1)
