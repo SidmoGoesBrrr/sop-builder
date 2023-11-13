640a446507417983936eab496f841e9d17e251f5
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-11-13 23:27:54 +0530

    Added a waiting for payment system

--- pages/5_💵_Payment.py
+++ pages/5_💵_Payment.py
@@ -3,27 +3,34 @@ import streamlit as st
 from pymongo import MongoClient
 import time
 import database
+import streamlit as st
 
 # Function to display the payment page
 def display_payment_page():
     # Razorpay payment link
     payment_link = "https://rzp.io/l/3FTyewKI8f"
+    
+    # Payment text
+    payment_text = "💰 Purchase one SOP for 99 rupees per SOP 💰"
+    
+    # Display payment text in bold with emoji
+    st.markdown(f"<p style='font-size: 24px; font-weight: bold;'>{payment_text}</p>", unsafe_allow_html=True)
 
-    # Displaying the payment button
-    if st.link_button('Proceed to Payment', payment_link):
-        # Open the payment link
-        st.markdown(f"<a href='{payment_link}' target='_blank'>Click here if you are not redirected</a>", unsafe_allow_html=True)
-        # Set a flag indicating the payment process has started
-        st.session_state['waiting_for_payment'] = True
+    # Function to open the payment link
+    def open_payment_link():
+        payment_link = "https://rzp.io/l/3FTyewKI8f"
+        js = f"window.open('{payment_link}')"  # JavaScript to open the link in a new tab
+        html = f"<script>{js}</script>"  # HTML to execute the JavaScript
+        st.markdown(html, unsafe_allow_html=True)  # Display the HTML
 
-    # Check if waiting for payment
-    if st.session_state.get('waiting_for_payment', False):
-        st.info("Waiting for payment confirmation... Please do not close this page.")
-
-# Initialize session state for payment waiting
-if 'waiting_for_payment' not in st.session_state:
-    st.session_state.waiting_for_payment = False
-
+    # Create a Streamlit button to open the payment link
+    if st.button("Pay now"):
+        st.write("If you are not redirected, please click here.")
+        st.session_state["payment_status"] = "waiting"
+        open_payment_link()
+        st.write("Waiting for payment...")
+        while st.session_state["payment_status"] == "waiting":
+            time.sleep(1)
 # Check if the user is logged in
 if st.session_state.get("user_logged_in"):
     display_payment_page()
