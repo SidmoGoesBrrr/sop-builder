d6835abdb112bc0d4b2b1a78a51b6e8516403fdb
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-11-13 23:31:25 +0530

    Made small tweaks

--- pages/5_💵_Payment.py
+++ pages/5_💵_Payment.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 import time
 import database
 import streamlit as st
-
+from streamlit.components.v1 import html
 # Function to display the payment page
 def display_payment_page():
     # Razorpay payment link
@@ -17,17 +17,20 @@ def display_payment_page():
     st.markdown(f"<p style='font-size: 24px; font-weight: bold;'>{payment_text}</p>", unsafe_allow_html=True)
 
     # Function to open the payment link
-    def open_payment_link():
-        payment_link = "https://rzp.io/l/3FTyewKI8f"
-        js = f"window.open('{payment_link}')"  # JavaScript to open the link in a new tab
-        html = f"<script>{js}</script>"  # HTML to execute the JavaScript
-        st.markdown(html, unsafe_allow_html=True)  # Display the HTML
+
+
+    def open_page(url):
+        open_script= """
+            <script type="text/javascript">
+                window.open('%s', '_blank').focus();
+            </script>
+        """ % (url)
+        html(open_script)
 
     # Create a Streamlit button to open the payment link
-    if st.button("Pay now"):
+    if st.button("Pay now".on_click(open_page, payment_link)):
         st.write("If you are not redirected, please click here.")
         st.session_state["payment_status"] = "waiting"
-        open_payment_link()
         st.write("Waiting for payment...")
         while st.session_state["payment_status"] == "waiting":
             time.sleep(1)
