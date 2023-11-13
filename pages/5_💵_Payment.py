ecf679a47b942bbc512c09337fe930ca25b9b156
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-11-14 00:04:46 +0530

    Unique code

--- pages/5_💵_Payment.py
+++ pages/5_💵_Payment.py
@@ -3,7 +3,10 @@ import streamlit as st
 from pymongo import MongoClient
 import time
 import database
-
+import random
+def generate_unique_code():
+    # Generate a random number between 100 and 999
+    return str(random.randint(100, 999))
 # Function to display the payment page
 def display_payment_page():
     # Razorpay payment link
@@ -11,18 +14,19 @@ def display_payment_page():
     if 'waiting_for_payment' not in st.session_state:
         st.session_state.waiting_for_payment = False
     # Displaying the payment button
-    if st.link_button('Proceed to Payment',payment_link):
-        # Open the payment link
-        st.markdown(f"<a href='{payment_link}' target='_blank'>Click here if you are not redirected</a>", unsafe_allow_html=True)
+    if st.button('Pay Now'):
+        
+        # Open the payment link in a new tab
+        unique_code = generate_unique_code()
+        st.markdown(f"<a href='{payment_link}?unique_code={unique_code}' target='_blank'>Click here to complete the payment</a>", unsafe_allow_html=True)
         # Set a flag indicating the payment process has started
         st.session_state['waiting_for_payment'] = True
-    if st.session_state['waiting_for_payment']:
-        # Display a message indicating the payment process has started
-        st.info("⏳ Waiting for payment...")
+
         # Redirect to the payment page
         while st.session_state['waiting_for_payment']==True:
+            st.info("⏳ Waiting for payment...")
             time.sleep(1)
-    
+
    
 
 # Initialize session state for payment waiting
