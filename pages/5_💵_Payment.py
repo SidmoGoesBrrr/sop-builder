125a1edd417646c7421730d70fb739c5d27e8e93
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-11-15 11:17:55 +0530

    Tried to fix payment checker

--- pages/5_💵_Payment.py
+++ pages/5_💵_Payment.py
@@ -21,7 +21,7 @@ def display_payment_page():
     
     if st.button('Buy Credits'):
         # Open the payment link in a new tab
-        st.link_button("Pay Now", payment_link)
+        st.link_button("Pay Now", payment_link,type="secondary",)
         # Set a flag indicating the payment process has started
         st.session_state['waiting_for_payment'] = True
         st.info("⏳ Waiting for payment...")
@@ -29,9 +29,18 @@ def display_payment_page():
         while st.session_state['waiting_for_payment']:
             sheet_url = pd.read_csv("https://docs.google.com/spreadsheets/d/1rs4dVdlLXi8c3kN0pRRegu-gKjIH6j1gw_3pyGUZJo0/export?gid=719810628&format=csv",
                                     index_col=0)
+            logging.info(sheet_url)
+            logging.info(sheet_url.index)
+            logging.info(database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0))
+            logging.info(sheet_url.loc[database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0), "Payment Status"])
+            
             # check if there is the required phone number in the sheet and if yes, check payment status of that phone number
             if database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0) in sheet_url.index:
-                if sheet_url.loc[database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0), "Payment Status"] == "paid":
+                payment_status = sheet_url.loc[database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0), "Payment Status"]
+                # Strip whitespace and newline characters from the payment status
+                payment_status = payment_status.strip()
+
+                if payment_status == "captured paid":
                     st.session_state['waiting_for_payment'] = False
                     st.session_state['payment_successful'] = True
                     # Update the user's SOP credits
