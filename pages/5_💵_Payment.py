7cad6f1fea8a924a8d13066441511bc6bfd644da
Siddhant Mohile <60550481+SidmoGoesBrrr@users.noreply.github.com>
2023-11-15 11:56:53 +0530

    Added time checker

--- pages/5_💵_Payment.py
+++ pages/5_💵_Payment.py
@@ -3,6 +3,8 @@ import streamlit as st
 from pymongo import MongoClient
 import time
 import database
+import pytz
+from datetime import datetime
 import random
 import streamlit as st
 import pandas as pd
@@ -30,14 +32,24 @@ def display_payment_page():
         # Redirect to the payment page
         while st.session_state['waiting_for_payment']:
             sheet_url = pd.read_csv("https://docs.google.com/spreadsheets/d/1rs4dVdlLXi8c3kN0pRRegu-gKjIH6j1gw_3pyGUZJo0/export?gid=719810628&format=csv",
-                                    index_col=0)
+                                    index_col=1)
             logging.info(sheet_url)
-            logging.info(sheet_url.index(1))
-            logging.info(database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0))
+            logging.info(sheet_url.index)
+            sop_db_ph_no=database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0)
+            razorpay_created_at=sheet_url.loc[sop_db_ph_no, 'Created At']
+            created_at_datetime = datetime.fromtimestamp(razorpay_created_at, tz=pytz.UTC)
+
+            # Get the current time in UTC
+            current_datetime_utc = datetime.now(pytz.UTC)
+
+            # Calculate the time difference in seconds
+            time_difference = (current_datetime_utc - created_at_datetime).total_seconds()
+            logging.info(time_difference)
+            logging.info(sop_db_ph_no)
             logging.info(sheet_url.loc)
             
             # check if there is the required phone number in the sheet and if yes, check payment status of that phone number
-            if database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0) in sheet_url.index:
+            if sop_db_ph_no in sheet_url.index and time_difference < 180: #3 minutes
                 payment_status = sheet_url.loc[database.get_user_data_by_id(st.session_state.user_id).get("phone_number", 0), "Payment Status"]
                 # Strip whitespace and newline characters from the payment status
                 payment_status = payment_status.strip()
@@ -54,6 +66,10 @@ def display_payment_page():
                     st.info("🔥 You can also view your SOP draft by clicking on the 'Drafts' button on the sidebar. 🔥")
                     break
 
+                elif payment_status == "failed":
+                    st.session_state['waiting_for_payment'] = False
+                    st.error("⚠️ Payment failed. Please try again. ⚠️")
+                    break
 
 if st.session_state.get("user_logged_in"):
     display_payment_page()
